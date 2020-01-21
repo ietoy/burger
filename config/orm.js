43dd7e880a28b3de936d7f242b9983ad9d4fe7b9
Ian Toy <iet@ela.com>
2020-01-20 18:07:46 -0800

    completed orm functionality for selectAll() and insertOne()

--- config/orm.js
+++ config/orm.js
@@ -5,19 +5,16 @@ var orm = {
         var queryString = "SELECT * FROM burgers";
         connection.query(queryString, function(err, result) {
             if (err) throw err;
-            console.log(result)
+            // console.log(result)
         })
     },
 
-    insertOne: function() {
-        // PLACEHOLDER QUERYSTRING
-        var queryString = " ";
-        // ADD [] FOR INTAKE PARAMETERS B/W QSTRING & FX
-        connection.query(queryString, function(err, result) {
+    insertOne: function(name) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, [name], function(err, result) {
             if (err) throw err;
-            // PLACEHOLDER WORK
-            console.log(result)
-        })
+            // console.log(result);
+        });
     },
 
     updateOne: function() {
@@ -31,5 +28,7 @@ var orm = {
         })
     }
 };
+
+
 
 module.exports = orm;
---
+++ server.js
@@ -0,0 +1,3 @@
+var orm = require("./config/orm.js");
+
+orm.insertOne("Second Test Burger");
